29b37ca438662e6f7f849354f27ada0ee2423856
kevin <kevin@hti-systems.co.za>
2022-05-17 08:59:22 +0200

    cloudbuild test take 1

--- cloudbuild/build.sh
+++ cloudbuild/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-lein change repositories set "[[\"artifact-registry\" {:url \"artifactregistry://europe-west1-maven.pkg.dev/hti-build/hti-maven\" :username \"_json_key_base64\" :password \"$MAVEN_PASSWORD\"}]]"
+echo $MAVEN_SETTINGS > ~/.m2/settings.xml
 
-lein with-profile prod ring uberjar
+./bin/build
