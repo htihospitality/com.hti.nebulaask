72b472c76744154ee9499acd28574c3e11af5bd5
kevin <kevin@hti-systems.co.za>
2022-05-17 14:00:25 +0200

    cloudbuild test take 4

--- cloudbuild/build.sh
+++ cloudbuild/build.sh
@@ -2,4 +2,10 @@
 
 echo $MAVEN_SETTINGS > ~/.m2/settings.xml
 
+echo @hti-ui:registry=https://europe-west1-npm.pkg.dev/hti-build/hti-npm/ > ~/.npmrc
+echo //europe-west1-npm.pkg.dev/hti-build/hti-npm/:_password=$GOOGLE_ARTIFACT_PASSWORD >> ~/.npmrc
+echo //europe-west1-npm.pkg.dev/hti-build/hti-npm/:username=_json_key_base64 >> ~/.npmrc
+echo //europe-west1-npm.pkg.dev/hti-build/hti-npm/:email=not.valid@email.com >> ~/.npmrc
+echo //europe-west1-npm.pkg.dev/hti-build/hti-npm/:always-auth=true >> ~/.npmrc
+
 ../bin/build
