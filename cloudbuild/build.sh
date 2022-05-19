4895928e26fe1b8235f716e8c9a643692bd2aa4b
kevin <kevin@hti-systems.co.za>
2022-05-19 15:05:08 +0200

    changed yarn install to not use npm

--- cloudbuild/build.sh
+++ cloudbuild/build.sh
@@ -4,9 +4,7 @@ export AWS_ACCESS_KEY_ID=$AWS_CREDENTIALS_ACCESS
 export AWS_SECRET_ACCESS_KEY=$AWS_CREDENTIALS_SECRET
 export AWS_DEFAULT_REGION=$AWS_REGION
 
-apt-get install nodejs
 
-npm install yarn
-
+sudo apt-get install yarn
 
 ../bin/build
