0e7cb8d2e076d488cab98efb1d6e6eb2001dca96
kevin <kevin@hti-systems.co.za>
2022-05-19 18:06:32 +0200

    try npm again X3

--- cloudbuild/build.sh
+++ cloudbuild/build.sh
@@ -5,7 +5,9 @@ export AWS_SECRET_ACCESS_KEY=$AWS_CREDENTIALS_SECRET
 export AWS_DEFAULT_REGION=$AWS_REGION
 
 
-apt install nodejs npm
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+apt-get install -y nodejs
+#apt install nodejs npm
 node -v
 npm install yarn
 yarn -v
