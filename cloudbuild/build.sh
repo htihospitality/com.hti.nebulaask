39ca12b1729df7963497605334c00cef38a0ff5f
kevin <kevin@hti-systems.co.za>
2022-05-19 09:05:49 +0200

    atemted to format the secret

--- cloudbuild/build.sh
+++ cloudbuild/build.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 mkdir ~/.aws
-echo $AWS_CREDENTIALS > ~/.aws/credentials
-echo $AWS_CONFIG > ~/.aws/config
+echo $AWS_CREDENTIALS | tr '_-' '/+' | base64 -d > ~/.aws/credentials
+echo $AWS_CONFIG | tr '_-' '/+' | base64 -d > ~/.aws/config
 
 cat ~/.aws/config
 
