6aeb5a20c2d6edc441e9a54563a87891f619cbb5
kevin <kevin@hti-systems.co.za>
2022-05-19 10:53:12 +0200

    set aws creds as environment settings

--- cloudbuild/build.sh
+++ cloudbuild/build.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-mkdir ~/.aws
-echo [default]  > ~/.aws/credentials
-echo aws_access_key_id = $AWS_CREDENTIALS_ACCESS  >> ~/.aws/credentials
-echo aws_secret_access_key = $AWS_CREDENTIALS_SECRET  >> ~/.aws/credentials
+#mkdir ~/.aws
+#echo [default]  > ~/.aws/credentials
+#echo aws_access_key_id = $AWS_CREDENTIALS_ACCESS  >> ~/.aws/credentials
+#echo aws_secret_access_key = $AWS_CREDENTIALS_SECRET  >> ~/.aws/credentials
 
-echo $AWS_CONFIG > ~/.aws/config
+export AWS_ACCESS_KEY_ID=$AWS_CREDENTIALS_ACCESS
+export AWS_SECRET_ACCESS_KEY=$AWS_CREDENTIALS_SECRET
+export AWS_DEFAULT_REGION=$AWS_REGION
 
-cat ~/.aws/credentials
+#echo $AWS_CONFIG > ~/.aws/config
+
+#cat ~/.aws/credentials
 
 ../bin/build
