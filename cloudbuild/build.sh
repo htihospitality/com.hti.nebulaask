29efd7047b340809d4dd4a01f8b21e0f484cbd38
kevin <kevin@hti-systems.co.za>
2022-05-19 14:57:39 +0200

    reverted docker image and installed node and then yarn

--- cloudbuild/build.sh
+++ cloudbuild/build.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-#mkdir ~/.aws
-#echo [default]  > ~/.aws/credentials
-#echo aws_access_key_id = $AWS_CREDENTIALS_ACCESS  >> ~/.aws/credentials
-#echo aws_secret_access_key = $AWS_CREDENTIALS_SECRET  >> ~/.aws/credentials
-
 export AWS_ACCESS_KEY_ID=$AWS_CREDENTIALS_ACCESS
 export AWS_SECRET_ACCESS_KEY=$AWS_CREDENTIALS_SECRET
 export AWS_DEFAULT_REGION=$AWS_REGION
 
-#echo $AWS_CONFIG > ~/.aws/config
+apt-get install nodejs
 
-#cat ~/.aws/credentials
+npm install yarn
+
 
 ../bin/build
