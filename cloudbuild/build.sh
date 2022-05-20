85a36ce1b5c7111b1f1b3572bffe63bfbdc70a62
kevin <kevin@hti-systems.co.za>
2022-05-20 09:39:04 +0200

    yaml changes test x 6

--- cloudbuild/build.sh
+++ cloudbuild/build.sh
@@ -6,8 +6,13 @@ export AWS_DEFAULT_REGION=$AWS_REGION
 
 apt update
 apt install -y nodejs npm
+echo "Node Version:"
 node -v
+echo "npm Version:"
+npm -v
+echo "installing yarn"
 npm install yarn
+echo "yarn version:"
 yarn -v
 
 #../bin/build
