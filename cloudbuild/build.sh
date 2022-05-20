e6f9cb89ee25ae626bb862a24175adfef23ac1c4
kevin <kevin@hti-systems.co.za>
2022-05-20 08:58:03 +0200

    yaml changes test x 3

--- cloudbuild/build.sh
+++ cloudbuild/build.sh
@@ -4,10 +4,12 @@ export AWS_ACCESS_KEY_ID=$AWS_CREDENTIALS_ACCESS
 export AWS_SECRET_ACCESS_KEY=$AWS_CREDENTIALS_SECRET
 export AWS_DEFAULT_REGION=$AWS_REGION
 
-apt-get install nodejs npm
+
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs npm
 node -v
 npm install yarn
 yarn -v
 
-../bin/build
+#../bin/build
 
