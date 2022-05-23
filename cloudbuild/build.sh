e54bb1b3b6c93421e906f8f1ff407ac8d95e458f
kevin <kevin@hti-systems.co.za>
2022-05-23 09:50:04 +0200

    added @hti-auth to .npmrc

--- cloudbuild/build.sh
+++ cloudbuild/build.sh
@@ -7,6 +7,7 @@ export AWS_DEFAULT_REGION=$AWS_REGION
 
 echo "Setting npm creds"
 echo @hti-ui:registry=https://europe-west1-npm.pkg.dev/hti-build/hti-npm/ > ~/.npmrc
+echo @hti-auth:registry=https://europe-west1-npm.pkg.dev/hti-build/hti-npm/ > ~/.npmrc
 echo //europe-west1-npm.pkg.dev/hti-build/hti-npm/:_password=$GOOGLE_ARTIFACT_PASSWORD >> ~/.npmrc
 echo //europe-west1-npm.pkg.dev/hti-build/hti-npm/:username=_json_key_base64 >> ~/.npmrc
 echo //europe-west1-npm.pkg.dev/hti-build/hti-npm/:email=not.valid@email.com >> ~/.npmrc
