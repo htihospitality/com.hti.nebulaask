7449f84e6d9554144302f7220993809c4971c5fa
kevin <kevin@hti-systems.co.za>
2022-05-23 08:04:02 +0200

    added .npmrc file

--- cloudbuild/build.sh
+++ cloudbuild/build.sh
@@ -4,18 +4,19 @@ export AWS_ACCESS_KEY_ID=$AWS_CREDENTIALS_ACCESS
 export AWS_SECRET_ACCESS_KEY=$AWS_CREDENTIALS_SECRET
 export AWS_DEFAULT_REGION=$AWS_REGION
 
+echo @hti-ui:registry=https://europe-west1-npm.pkg.dev/hti-build/hti-npm/ > ~/.npmrc
+echo //europe-west1-npm.pkg.dev/hti-build/hti-npm/:_password=$GOOGLE_ARTIFACT_PASSWORD >> ~/.npmrc
+echo //europe-west1-npm.pkg.dev/hti-build/hti-npm/:username=_json_key_base64 >> ~/.npmrc
+echo //europe-west1-npm.pkg.dev/hti-build/hti-npm/:email=not.valid@email.com >> ~/.npmrc
+echo //europe-west1-npm.pkg.dev/hti-build/hti-npm/:always-auth=true >> ~/.npmrc
+
 apt update
 apt ugrade -Y
 
 apt install -y nodejs npm
-#echo "Node Version:"
-#node -v
-#echo "npm Version:"
-#npm -v
-echo "installing yarn"
-npm install yarn --force
-echo "yarn version:"
-yarn -v
 
-#../bin/build
+npm install yarn --force
+
+
+../bin/build
 
